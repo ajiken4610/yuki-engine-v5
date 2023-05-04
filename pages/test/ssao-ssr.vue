246a72527fddfc7fc62131b5047f13ea91121daa
ajiken4610 <github>
2023-05-04 16:24:20 +0900

    ssr実装完了

---
+++ pages/test/ssao-ssr.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+div
+  canvas.load-scene(ref="canvasRef")
+</template>
+
+<script setup lang="ts">
+import { SSAORenderingPipeline, SSRRenderingPipeline, SceneLoader } from 'babylonjs'
+import modelPath from "assets/test.glb?url"
+import 'babylonjs-loaders';
+// import { MeshBuilder } from 'babylonjs';
+const canvasRef = ref<HTMLCanvasElement>()
+onMounted(() => {
+  const canvas = canvasRef.value!
+  const engine = createEngine(canvas)
+  const scene = createScene(engine)
+  scene.clearColor.set(0, 0, 0, 1)
+  const ssao = new SSAORenderingPipeline("ssao", scene, .75)
+  const ssr = new SSRRenderingPipeline("ssr", scene)
+  // ssr.debug = true
+  // SceneLoader.ShowLoadingScreen = false
+  // const box = MeshBuilder.CreateBox("box", undefined, scene)
+  // box.position.x = 3
+  SceneLoader.AppendAsync(modelPath, undefined, scene).then(() => {
+    const camera = scene.getCameraByName("Camera.001")!
+    scene.activeCamera = camera;
+    camera.attachControl(canvas)
+    addShadowToScene(scene, "", { blur: false })
+    scene.postProcessRenderPipelineManager.attachCamerasToRenderPipeline("ssao", camera);
+    scene.postProcessRenderPipelineManager.attachCamerasToRenderPipeline("ssr", camera);
+    engine.runRenderLoop(() => {
+      scene.render();
+    });
+  })
+
+  onUnmounted(() => {
+    ssao.dispose()
+    ssr.dispose()
+    engine.dispose()
+  })
+})
+
+</script>
+
+<style lang="scss">
+html,
+body {
+  margin: 0 auto;
+}
+</style>
+
+<style scoped lang="scss">
+.load-scene {
+  height: 90vh;
+  width: 100vw;
+}
+</style>
