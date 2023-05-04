ff65e497f06b0cc5cc9783ddb6c1a31810371909
ajiken4610 <github>
2023-05-04 15:56:49 +0900

    影の関数化

---
+++ utils/addShadowToScene.ts
@@ -0,0 +1,33 @@
+import { Scene, ShadowGenerator } from "babylonjs";
+
+export default (
+  scene: Scene,
+  keyword: string = "",
+  options: {
+    blur: boolean;
+    size: number;
+    fullFloat: boolean;
+  } = {}
+) => {
+  scene.lights.forEach((light) => {
+    // light.intensity *= .1
+    if (light.name.indexOf(keyword) === -1) {
+      return;
+    }
+    const shadowGenerator = new ShadowGenerator(
+      options.size || 1024,
+      light as IShadowLight,
+      !!options.fullFloat
+    );
+    shadowGenerator.useBlurExponentialShadowMap = !!options.blur;
+    shadowGenerator.useKernelBlur = true;
+    shadowGenerator.blurKernel = 8;
+    shadowGenerator.bias = 0.0001;
+    scene.meshes.forEach((mesh) => {
+      shadowGenerator.addShadowCaster(mesh, false);
+    });
+  });
+  scene.meshes.forEach((mesh) => {
+    mesh.receiveShadows = true;
+  });
+};
