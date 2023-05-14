8ea88b53a988d55468a6bc9a0d31ef3b0f1195d4
ajiken4610 <ajiken4610@gmail.com>
2023-05-14 17:19:28 +0900

    DynamicStringResourceのリライトとテストケース完了

---
+++ pages/test/dyn-str1.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+div see console
+</template>
+
+<script setup lang="ts">
+const result = await useDynamicStringReference("hello@/test/dynStr.js", {}, {});
+console.log(result);
+</script>
---
+++ pages/test/dyn-str2.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+div see console
+</template>
+
+<script setup lang="ts">
+const result = await useDynamicStringReference(
+  "hello@/test/dynStr.json",
+  {},
+  { lang: "ja", region: "jp" }
+);
+console.log(result);
+</script>
