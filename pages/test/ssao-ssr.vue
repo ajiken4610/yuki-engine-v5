82bc047d3a51f8ec35136a585ca4c058ff2a5f99
ajiken4610 <ajiken4610@gmail.com>
2023-05-14 18:19:36 +0900

    エンジンをWebGPUに対応

--- pages/test/ssao-ssr.vue
+++ pages/test/ssao-ssr.vue
@@ -4,41 +4,55 @@ div
 </template>
 
 <script setup lang="ts">
-import { SSAORenderingPipeline, SSRRenderingPipeline, SceneLoader } from 'babylonjs'
-import modelPath from "assets/test.glb?url"
-import 'babylonjs-loaders';
+import {
+  Engine,
+  SSAORenderingPipeline,
+  SSRRenderingPipeline,
+  SceneLoader,
+} from "babylonjs";
+import modelPath from "assets/test.glb?url";
+import "babylonjs-loaders";
 // import { MeshBuilder } from 'babylonjs';
-const canvasRef = ref<HTMLCanvasElement>()
+const canvasRef = ref<HTMLCanvasElement>();
 onMounted(() => {
-  const canvas = canvasRef.value!
-  const engine = createEngine(canvas)
-  const scene = createScene(engine)
-  scene.clearColor.set(0, 0, 0, 1)
-  const ssao = new SSAORenderingPipeline("ssao", scene, .75)
-  const ssr = new SSRRenderingPipeline("ssr", scene)
-  // ssr.debug = true
-  // SceneLoader.ShowLoadingScreen = false
-  // const box = MeshBuilder.CreateBox("box", undefined, scene)
-  // box.position.x = 3
-  SceneLoader.AppendAsync(modelPath, undefined, scene).then(() => {
-    const camera = scene.getCameraByName("Camera.001")!
-    scene.activeCamera = camera;
-    camera.attachControl(canvas)
-    addShadowToScene(scene, "", { blur: false })
-    scene.postProcessRenderPipelineManager.attachCamerasToRenderPipeline("ssao", camera);
-    scene.postProcessRenderPipelineManager.attachCamerasToRenderPipeline("ssr", camera);
-    engine.runRenderLoop(() => {
-      scene.render();
+  const canvas = canvasRef.value!;
+  let outerEngine: Engine;
+  createEngine(canvas).then((engine) => {
+    outerEngine = engine;
+    const scene = createScene(engine);
+    scene.clearColor.set(0, 0, 0, 1);
+    new SSAORenderingPipeline("ssao", scene, 0.75);
+    new SSRRenderingPipeline("ssr", scene);
+    // ssr.debug = true
+    // SceneLoader.ShowLoadingScreen = false
+    // const box = MeshBuilder.CreateBox("box", undefined, scene)
+    // box.position.y = 3
+    SceneLoader.AppendAsync(modelPath, undefined, scene).then(() => {
+      const camera = scene.getCameraByName("Camera.001")!;
+      scene.activeCamera = camera;
+      camera.attachControl(canvas);
+      addShadowToScene(scene, "", { blur: true });
+      scene.lights.forEach((light) => {
+        light.intensity *= 0.1;
+      });
+      scene.postProcessRenderPipelineManager.attachCamerasToRenderPipeline(
+        "ssao",
+        camera
+      );
+      scene.postProcessRenderPipelineManager.attachCamerasToRenderPipeline(
+        "ssr",
+        camera
+      );
+      engine.runRenderLoop(() => {
+        scene.render();
+      });
     });
-  })
+  });
 
   onUnmounted(() => {
-    ssao.dispose()
-    ssr.dispose()
-    engine.dispose()
-  })
-})
-
+    outerEngine.dispose();
+  });
+});
 </script>
 
 <style lang="scss">
